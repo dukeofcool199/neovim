f125060715879003455c2ad81f577af9c67d7ff0
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-08-22 16:15:54 -0600

    feature/close single buffer with qq, force quit with qQ

--- packages/neovim/lua/quit.lua
+++ packages/neovim/lua/quit.lua
@@ -1,5 +1,6 @@
 local which_key = require("which-key")
 
 which_key.register({
-		q = { q = {"<cmd>q!<cr>", "Force Quit"}, w = {"<cmd>wa<cr> <cmd>q!<cr>", "Save and Quit"} }
+	q = { Q = { "<cmd>q!<cr>", "Force Quit" }, w = { "<cmd>wa<cr> <cmd>q!<cr>", "Save and Quit" },
+		q = { "<cmd>q<cr>", "Close buffer" } }
 }, { mode = "n", prefix = "<leader>", silent = true })
