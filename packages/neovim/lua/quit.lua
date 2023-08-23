5c8fbfcd5654b7d739ac327ca0711b559f0cbcf0
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-08-23 07:17:18 -0600

    add keybinding to close all

--- packages/neovim/lua/quit.lua
+++ packages/neovim/lua/quit.lua
@@ -3,7 +3,8 @@ local which_key = require("which-key")
 which_key.register({
 	q = {
 		Q = { "<cmd>q!<cr>", "Force Quit" },
+		a = { "<cmd>qa!<cr>", "Force Quit All" }
 		w = { "<cmd>wa<cr> <cmd>q!<cr>", "Save and Quit" },
-		q = { "<cmd>q<cr>", "Close buffer" }
+		q = { "<cmd>q<cr>", "Close buffer" },
 	}
 }, { mode = "n", prefix = "<leader>", silent = true })
