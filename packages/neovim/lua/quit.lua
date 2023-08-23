6337c9b4d2aecbb53989c81546d6eb2be2d31df6
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-08-23 07:16:21 -0600

    formatting

--- packages/neovim/lua/quit.lua
+++ packages/neovim/lua/quit.lua
@@ -1,6 +1,9 @@
 local which_key = require("which-key")
 
 which_key.register({
-	q = { Q = { "<cmd>q!<cr>", "Force Quit" }, w = { "<cmd>wa<cr> <cmd>q!<cr>", "Save and Quit" },
-		q = { "<cmd>q<cr>", "Close buffer" } }
+	q = {
+		Q = { "<cmd>q!<cr>", "Force Quit" },
+		w = { "<cmd>wa<cr> <cmd>q!<cr>", "Save and Quit" },
+		q = { "<cmd>q<cr>", "Close buffer" }
+	}
 }, { mode = "n", prefix = "<leader>", silent = true })
