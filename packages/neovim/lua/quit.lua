cd40d037faab00b7d568e74bd3f429cf15004774
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-08-23 07:23:16 -0600

    keys/quit force quit all bindings

--- packages/neovim/lua/quit.lua
+++ packages/neovim/lua/quit.lua
@@ -3,7 +3,7 @@ local which_key = require("which-key")
 which_key.register({
 	q = {
 		Q = { "<cmd>q!<cr>", "Force Quit" },
-		a = { "<cmd>qa!<cr>", "Force Quit All" }
+		a = { "<cmd>qa!<cr>", "Force Quit All" },
 		w = { "<cmd>wa<cr> <cmd>q!<cr>", "Save and Quit" },
 		q = { "<cmd>q<cr>", "Close buffer" },
 	}
