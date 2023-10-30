3e79b05cd31dfed387f85f2a234670c68b6d6920
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-10-30 14:38:53 -0600

    disable virtual text

--- packages/neovim/lua/lspconfig.lua
+++ packages/neovim/lua/lspconfig.lua
@@ -327,7 +327,7 @@ prettier.setup {
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	underline = true,
 	update_in_insert = false,
-	virtual_text = { spacing = 4, prefix = "●" },
+	virtual_text = false,
 	severity_sort = true,
 })
 
