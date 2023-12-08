a75c6ba876f1111f215f5d71789002a6439cc806
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-12-07 21:40:34 -0700

    add lsp restart which key registration

--- packages/neovim/lua/which-key.lua
+++ packages/neovim/lua/which-key.lua
@@ -2,25 +2,29 @@ local which_key = require("which-key")
 
 which_key.setup {}
 
+
 which_key.register({
-	b = {
-		name = "Buffer",
-		d = { "<cmd>:BD<cr>", "Delete Buffer" },
-		n = { "<cmd>:bnext<cr>", "Next Buffer" },
-		p = { "<cmd>:bprevious<cr>", "Previous Buffer" },
-	},
-	t = {
-		name = "Toggle",
-		h = {
-			function()
-				vim.o.hlsearch = not vim.o.hlsearch
-			end,
-			"Highlight"
-		},
-	},
-	s = {
-		name = "Split",
-		v = { "<cmd>:vsplit<cr>", "vertical split" },
-		h = { "<cmd>:split<cr>", "horizontal split" }
-	}
+  r = {
+    "<cmd>:LspRestart<cr>", "restart LSP"
+  },
+  b = {
+    name = "Buffer",
+    d = { "<cmd>:BD<cr>", "Delete Buffer" },
+    n = { "<cmd>:bnext<cr>", "Next Buffer" },
+    p = { "<cmd>:bprevious<cr>", "Previous Buffer" },
+  },
+  t = {
+    name = "Toggle",
+    h = {
+      function()
+        vim.o.hlsearch = not vim.o.hlsearch
+      end,
+      "Highlight"
+    },
+  },
+  s = {
+    name = "Split",
+    v = { "<cmd>:vsplit<cr>", "vertical split" },
+    h = { "<cmd>:split<cr>", "horizontal split" }
+  }
 }, { mode = "n", prefix = "<leader>" })
