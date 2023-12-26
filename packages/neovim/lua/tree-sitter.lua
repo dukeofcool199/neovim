a419d7909e2c13ea73f93aec1ec630cdddbd5dd4
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-12-25 22:10:58 -0700

    treesitter highlighting

--- packages/neovim/lua/tree-sitter.lua
+++ packages/neovim/lua/tree-sitter.lua
@@ -2,9 +2,12 @@ local which_key = require("which-key")
 local treesitter_configs = require("nvim-treesitter.configs")
 
 treesitter_configs.setup {
-	indent = {
-		enable = true,
-	},
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true,
+  },
 }
 
 vim.o.foldmethod = "expr"
@@ -14,14 +17,14 @@ vim.o.foldenable = false
 vim.o.foldlevel = 99
 
 which_key.register({
-	g = {
-		name = "Go",
-		H = {
-			"<cmd>TSHighlightCapturesUnderCursor<cr>", "Show Tree-sitter Captures"
-		},
-	},
-	t = {
-		name = "Toggle",
-		p = { "<cmd>TSPlaygroundToggle<cr>", "Toggle Tree-sitter Playground" }
-	}
+  g = {
+    name = "Go",
+    H = {
+      "<cmd>TSHighlightCapturesUnderCursor<cr>", "Show Tree-sitter Captures"
+    },
+  },
+  t = {
+    name = "Toggle",
+    p = { "<cmd>TSPlaygroundToggle<cr>", "Toggle Tree-sitter Playground" }
+  }
 }, { mode = "n", noremap = true, silent = true })
