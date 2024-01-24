e01ac1a69c852ba8f88914cb527293a51a246d8a
Jenkin Schibel <jenkin.schibel@protonmail.com>
2024-01-24 10:46:19 -0700

    fix git commit since fugitive remove GCommit

--- packages/neovim/lua/git.lua
+++ packages/neovim/lua/git.lua
@@ -3,15 +3,15 @@ local which_key = require("which-key")
 vim.api.nvim_create_augroup("bufcheck", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
-	group = "bufcheck",
-	pattern = { "gitcommit", "gitrebase" },
-	command = "startinsert | 1",
+  group = "bufcheck",
+  pattern = { "gitcommit", "gitrebase" },
+  command = "startinsert | 1",
 })
 
 which_key.register({
-	g = {
-		name = "Git",
-		w = { "<cmd>:Gwrite<cr>", "Git write" },
-		c = { "<cmd>:GCommit<cr>", "Git commit" }
-	}
+  g = {
+    name = "Git",
+    w = { "<cmd>:Gwrite<cr>", "Git write" },
+    c = { "<cmd>:G commit<cr>", "Git commit" }
+  }
 }, { mode = "n", prefix = "<leader>" })
