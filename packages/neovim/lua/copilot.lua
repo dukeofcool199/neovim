feb465c615316a91c87ed1e9a2107445e32265a4
Jenkin Schibel <jenkin.schibel@protonmail.com>
2024-02-06 09:17:07 -0700

    disable txt files for copilot

--- packages/neovim/lua/copilot.lua
+++ packages/neovim/lua/copilot.lua
@@ -1,52 +1,54 @@
 local which_key = require("which-key")
 
 require('copilot').setup({
-	panel = {
-		enabled = true,
-		auto_refresh = false,
-		keymap = {
-			jump_prev = "[[",
-			jump_next = "]]",
-			accept = "<CR>",
-			refresh = "gr",
-			open = "<M-CR>"
-		},
-		layout = {
-			position = "right", -- | top | left | right
-			ratio = 0.4
-		},
-	},
-	suggestion = {
-		enabled = true,
-		auto_trigger = true,
-		debounce = 75,
-		keymap = {
-			accept = "<C-A>",
-			accept_word = false,
-			accept_line = false,
-			next = "<C-]>",
-			prev = "<C-[>",
-		},
-	},
-	filetypes = {
-		yaml = false,
-		markdown = false,
-		help = false,
-		gitcommit = false,
-		gitrebase = false,
-		hgcommit = false,
-		svn = false,
-		cvs = false,
-		["."] = false,
-	},
-	copilot_node_command = 'node', -- Node.js version must be > 18.x
-	server_opts_overrides = {},
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "right", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<C-A>",
+      accept_word = false,
+      accept_line = false,
+      next = "<C-]>",
+      prev = "<C-[>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    text = false,
+    txt = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 18.x
+  server_opts_overrides = {},
 })
 
 which_key.register({
-	p = {
-		name = "Copilot",
-		p = { "<cmd>:Copilot panel<CR>", "toggle copilot panel" },
-		s = { "<cmd>:Copilot suggestion<CR>", "toggle copilot suggestsions" }
-	}
+  p = {
+    name = "Copilot",
+    p = { "<cmd>:Copilot panel<CR>", "toggle copilot panel" },
+    s = { "<cmd>:Copilot suggestion<CR>", "toggle copilot suggestsions" }
+  }
 }, { mode = "n", prefix = "<leader>", noremap = true, silent = true })
