78870238faaefb8ec2316439c2f5c484ca251cd9
Jenkin Schibel <jenkin.schibel@protonmail.com>
2024-02-06 09:17:17 -0700

    change comments for todo comments

--- packages/neovim/lua/todo-comments.lua
+++ packages/neovim/lua/todo-comments.lua
@@ -1,23 +1,23 @@
 require("todo-comments").setup {
-	keywords = {
-		FIX = {
-			icon = " ", -- icon used for the sign, and in search results
-			color = "error", -- can be a hex color, or a named color (see below)
-			alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-			-- signs = false, -- configure signs for some keywords individually
-		},
-		TODO = { icon = " ", color = "info" },
-		HACK = { icon = " ", color = "warning", alt = { "HACK", "DRAGONS" } },
-		WARN = { icon = " ", color = "warning", alt = { "WARNING", "WARNING", "WARN" } },
-		PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "PERF", "PERFORMANCE" } },
-		NOTE = { icon = " ", color = "hint", alt = { "INFO", "NOTE", "INFO" } },
-	},
-	merge_keywords = false,
-	highlight = {
-		pattern = [[.*(@)?(KEYWORDS)\(\s*\)\s*]],
-		after = ""
-	},
-	search = {
-		pattern = [[\b@(KEYWORDS)(\(\s*\))\s*:]]
-	}
+  keywords = {
+    FIX = {
+      icon = "💔", -- icon used for the sign, and in search results
+      color = "error", -- can be a hex color, or a named color (see below)
+      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+      -- signs = false, -- configure signs for some keywords individually
+    },
+    TODO = { icon = "💰", color = "info" },
+    HACK = { icon = "🌪️", color = "warning", alt = { "HACK", "DRAGONS" } },
+    WARN = { icon = "⛈️", color = "warning", alt = { "WARNING", "WARNING", "WARN" } },
+    PERF = { icon = "🐢", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "PERF", "PERFORMANCE" } },
+    NOTE = { icon = "🗒️", color = "hint", alt = { "INFO", "NOTE", "INFO" } },
+  },
+  merge_keywords = false,
+  highlight = {
+    pattern = [[.*(@)?(KEYWORDS)\(\s*\)\s*]],
+    after = ""
+  },
+  search = {
+    pattern = [[\b@(KEYWORDS)(\(\s*\))\s*:]]
+  }
 }
