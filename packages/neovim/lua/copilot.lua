b1968111445f50429057624555646dd1689c54c6
Jenkin Schibel <jenkin.schibel@protonmail.com>
2024-06-05 17:48:34 -0600

    disable copilot

--- packages/neovim/lua/copilot.lua
+++ packages/neovim/lua/copilot.lua
@@ -17,8 +17,8 @@ require('copilot').setup({
     },
   },
   suggestion = {
-    enabled = true,
-    auto_trigger = true,
+    enabled = false,
+    auto_trigger = false,
     debounce = 75,
     keymap = {
       accept = "<C-A>",
