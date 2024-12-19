6d409b68e4c12347526973b2697af6526a4c8b61
Jenkin Schibel <jenkin.schibel@protonmail.com>
2024-12-19 14:28:42 -0700

    change keybinding for toggling zen mode

--- packages/neovim/lua/zen.lua
+++ packages/neovim/lua/zen.lua
@@ -2,8 +2,8 @@ local which_key = require("which-key")
 
 
 which_key.register({
-	t = {
-		name = "Toggle",
-		z = { "<cmd>:ZenMode<cr>", "toggle zen mode" },
-	},
+  z = {
+    name = "Zen mode",
+    z = { "<cmd>:ZenMode<cr>", "toggle zen mode" },
+  },
 }, { mode = "n", prefix = "<leader>" })
