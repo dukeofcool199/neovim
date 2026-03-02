bce38aa04dbb6c5499420ed73f7319f6b809b1f5
Jenkin Schibel <jenkin.schibel@protonmail.com>
2026-03-02 14:00:21 -0700

    deps: update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Jenkin Schibels Neovim configuration - Nixvim";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     nixvim = {
       url = "github:nix-community/nixvim";
@@ -10,24 +10,25 @@
     };
   };
 
-  outputs = { nixpkgs, nixvim, ... }:
-    let
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+  outputs = {
+    nixpkgs,
+    nixvim,
+    ...
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      nixvim' = nixvim.legacyPackages.${system};
+      config = import ./config {inherit pkgs;};
+      nvim = nixvim'.makeNixvimWithModule {
+        inherit pkgs;
+        module = config;
+      };
     in {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-          config = import ./config { inherit pkgs; };
-          nvim = nixvim'.makeNixvimWithModule {
-            inherit pkgs;
-            module = config;
-          };
-        in {
-          default = nvim;
-          neovim = nvim;
-        });
-    };
+      default = nvim;
+      neovim = nvim;
+    });
+  };
 }
