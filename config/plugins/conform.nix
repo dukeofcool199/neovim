04ca60a035eea7d1b7d359018848f9133cc49654
Jenkin Schibel <jenkin@perceptview.com>
2026-03-11 16:07:13 -0600

    feat: add conform support

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -1,33 +1,30 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   plugins.conform-nvim = {
     enable = true;
 
     settings = {
-      formatters_by_ft = {
-        javascript = [ "prettier" ];
-        typescript = [ "prettier" ];
-        javascriptreact = [ "prettier" ];
-        typescriptreact = [ "prettier" ];
-        vue = [ "prettier" ];
-        html = [ "prettier" ];
-        css = [ "prettier" ];
-        json = [ "prettier" ];
-        yaml = [ "prettier" ];
-        markdown = [ "prettier" ];
-      };
+      notify_on_error = true;
 
       format_on_save = {
-        timeout_ms = 500;
+        timeout_ms = 4000;
         lsp_fallback = true;
       };
 
-      notify_on_error = true;
+      formatters_by_ft = {
+        javascript = ["standardjs"];
+        javascriptreact = ["standardjs"];
+        typescript = ["standardjs"];
+        typescriptreact = ["standardjs"];
+
+        nix = ["alejandra"];
+        lua = ["stylua"];
+      };
     };
   };
 
-  # Make prettier available in PATH
   extraPackages = with pkgs; [
-    nodePackages.prettier
+    nodejs
+    alejandra
+    stylua
   ];
 }
